53b182740593b3e1691f5eff4ef80f895512a68b
Himanshu Kewat <himanshukewat2810@gmail.com>
2026-06-04 17:47:39 +0530

    Add files via upload

---
+++ SingleNumber.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int xor = 0;
+        for(int i=0; i<nums.length; i++){
+            xor = xor ^ nums[i];
+        }
+        return xor;
+    }
+}
+
+
+// public int singleNumber(int[] nums) {
+//         HashMap<Integer,Integer> map = new HashMap<>();
+//         for(int i=0; i<nums.length; i++){
+//             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+//         }
+
+//         for(int key : map.keySet()){
+//             if(map.get(key) == 1){
+//                 return key;
+//             }
+//         }
+//         return -1;
+//     }
