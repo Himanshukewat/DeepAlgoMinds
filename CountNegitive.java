4dd64c33d9ff16764d96f60c70f3b1a1102ff075
Himanshu Kewat <himanshukewat2810@gmail.com>
2026-06-10 00:04:45 +0530

    Add files via upload
    https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/submissions/2027011300/

---
+++ CountNegitive.java
@@ -0,0 +1,21 @@
+public class CountNegitive {
+    public int countNegatives(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        int row = n-1;
+        int col = 0;
+        int cnt = 0;
+
+        while(row >= 0 && col < m){
+            if(grid[row][col] < 0){
+                cnt += m - col;
+                row -=1;
+            }
+            else {
+                col += 1;
+            }
+        }
+        return cnt;
+    }
+}
